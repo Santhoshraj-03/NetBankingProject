14fb4e229b6bb809d8428b0d5e3089010cc86636
Santhosh Kumar R <santhoshraj3101@gmail.com>
2024-05-09 14:06:50 +0530

    modified cheque controller

--- NetBankingProject/src/main/java/com/mindgate/main/controller/ChequeDetailsController.java
+++ NetBankingProject/src/main/java/com/mindgate/main/controller/ChequeDetailsController.java
@@ -2,6 +2,7 @@ package com.mindgate.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class ChequeDetailsController {
 		return chequeDetailsService.addChequeDeposit(chequeDetails);
 	}
 
+	@GetMapping("get-all-cheque")
 	public ResponseEntity<?> getAllChequeDetails() {
 		return chequeDetailsService.getAllChequeDeposits();
 	}
